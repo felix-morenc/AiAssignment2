ac769675035a0a043a4b61f2c01ae297683880e5
Felix Morenc <89635370+felix-morenc@users.noreply.github.com>
2023-11-10 16:43:44 +0100

    Update classification.py

--- classification.py
+++ classification.py
@@ -12,12 +12,6 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
-from scipy.stats import randint
-
-# Tree Visualisation
-from sklearn.tree import export_graphviz
-from IPython.display import Image
-import graphviz
 
 # fetch dataset 
 rice_cammeo_and_osmancik = fetch_ucirepo(id=545) 
